e9a53346e2acda27e77ec3f5f20cad1ba042efe1
zzuwenjie <wen20121724@163.com>
2018-10-19 16:30:38 +0800

    add moduel query_goods

--- app/src/main/java/com/xhzh/shounaxiang/activity/QueryGoodsActivity.java
+++ app/src/main/java/com/xhzh/shounaxiang/activity/QueryGoodsActivity.java
@@ -2,14 +2,27 @@ package com.xhzh.shounaxiang.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.xhzh.shounaxiang.R;
+import com.xhzh.shounaxiang.listener.OnClickBackListener;
+import com.xhzh.shounaxiang.util.AppUtils;
 
 public class QueryGoodsActivity extends AppCompatActivity {
-
+    ImageView iv_back;
+    EditText et_query_goods;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_goods);
+        initView();
+        AppUtils.showSoftInputFromWindow(this, et_query_goods);
+
+    }
+    void initView() {
+        iv_back = findViewById(R.id.iv_back);
+        iv_back.setOnClickListener(new OnClickBackListener(this));
+        et_query_goods = findViewById(R.id.et_query_goods);
     }
 }
